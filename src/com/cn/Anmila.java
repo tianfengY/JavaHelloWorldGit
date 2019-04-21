4f761390d2535b760eda22255f90cb4663d89076
tianfengY <981073945@qq.com>
2019-04-21 11:47:32 +0800

    1，idea github  java helloworld 2，增加Person类，main中增加控制台打印 3,增加anmial

---
+++ src/com/cn/Anmila.java
@@ -0,0 +1,13 @@
+package com.cn;
+
+public class Anmila {
+    public String dog;
+
+    public String getDog() {
+        return dog;
+    }
+
+    public void setDog(String dog) {
+        this.dog = dog;
+    }
+}
