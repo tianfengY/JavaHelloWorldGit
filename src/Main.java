4898878b646b4b9992c009f765a7d2a567ab950a
tianfengY <981073945@qq.com>
2019-04-21 11:18:07 +0800

    1，idea github  java helloworld 2，增加Person类，main中增加控制台打印

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        testStr();
+    }
+
+    //demo methor
+    public static void testStr(){
+
+        for(int i=0;i<10 ;i++ ){
+            System.out.println("加油中国！");
+        }
     }
 }
---
+++ src/tf/demo/Person.java
@@ -0,0 +1,31 @@
+package tf.demo;
+
+public class Person {
+    public String name;
+    public String age;
+    public String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
